8819a190f0797e7106123c1795a1b51ed49134f7
punithkumar-meraki <75274130+punithkumar-meraki@users.noreply.github.com>
2020-12-20 21:12:23 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,55 +1,41 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "./styles.css";
-var emojiDictionary ={
-  "A":"Apple",
-  "B":"Ball",
-  "C":"Cat",
-  "D":"Dog",
+var emojiDictionary = {
+  A: "Apple",
+  B: "Ball",
+  C: "Cat",
+  D: "Dog"
 };
 
-var emojisweknow =Object.keys(emojiDictionary);
+var emojisweknow = Object.keys(emojiDictionary);
 export default function App() {
-  var [meaning, setMeaning] = useState("")
+  var [meaning, setMeaning] = useState("");
 
-  function inputHandler(event){
+  function inputHandler(event) {
     var userInput = event.target.value;
     var meaning = emojiDictionary[userInput];
     setMeaning(meaning);
-
   }
-function emojiClickHandler(emoji){
-  var meaning = emojiDictionary[emoji];
-  setMeaning(meaning);
-
-}
-  
-
-
-
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
 
   return (
     <div className="App">
       <h1>Know More</h1>
       <input onChange={inputHandler} />
       <h2>{meaning}</h2>
-      {emojisweknow.map(function(emoji){
-        return(
-         <span onClick ={()=>emojiClickHandler(emoji)}
-         style={{padding:"1rem",fontsize:"1rem",cursor:"pointer"}}
-         >{emoji}
-         </span>
-          
-
+      {emojisweknow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ padding: "1rem", fontsize: "1rem", cursor: "pointer" }}
+          >
+            {emoji}
+          </span>
         );
       })}
-
-      
-     
-      
-      
-      
-      
-  
     </div>
   );
 }
